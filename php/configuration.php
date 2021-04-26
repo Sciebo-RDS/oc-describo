c06ac0df3424b6b25657dc6923b48215b23ac368
root <root@DESKTOP-6UJ2HIE.localdomain>
2021-04-26 14:22:00 +0200

    add doc for configuration.php

--- php/configuration.php
+++ php/configuration.php
@@ -4,10 +4,22 @@ namespace OCA\Describo;
 
 ### EDIT BELOW ###
 
+# Owncloud will use this url to exchange the user information against a sessionId.
+# This url have to be available for the ownCloud instance through local network.
 const apiURL = "http://api:8080/session/application";
+
+# Owncloud will use this url as the iframe source.
+# This url have to be available for the user of ownCloud / describo from public network.
 const uiURL = "http://localhost:9000";
+
+# This have to be the same secret, which you specified in describo configuration
 const describoSecretKey = "IAMSECRET";
+
+# This field can be changed, if you do not want to use the default oauth2 name for describo client.
 const oauthname = "describo";
+
+# This field sets the url, which will be opened, when you click on the "i" on the describo admin page next to the site title.
+# Helpful for shortcuts.
 const documentation = "https://github.com/Arkisto-Platform/describo-online";
 
 ### EDIT ABOVE ###
